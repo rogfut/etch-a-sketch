c7ea5df8f92664b17c5d3b9bfad5c273d2f50c2c
rogfut <rogfut@outlook.com>
2016-03-13 20:50:54 -0700

    Reworking the grid creation, update 1

--- js/main.js
+++ js/main.js
@@ -1,46 +1,70 @@
+
+
 $(document).ready(function() {
+   createGrid(); 
+});
+   
+//creates a grid based on user input
+function createGrid () {
     
-var box = prompt("Number of boxes >= 16");
-if  (box === null) {
-    var box = 100
-} else if (Math.sqrt(box) < 4) {
-    var box = prompt("Number of boxes must equal or greater than 16!");
+    //asks user to input a number, if no number is given value is set at 10    
+    var numSquares = prompt("Input a number between 1 and 32");
+    if  (numSquares === null) {
+        var numSquares = 10
+    } else if (numSquares > 32 || numSquares < 1 ) {
+        do { numSquares = prompt("Input a number between 1 and 32") }
+        while (numSquares > 32 || numSquares < 1)
+    }
+
+    //creates the square divs
+    for(i = 0; i < (numSquares * numSquares); i++) {
+        $("#grid").append("<div class='square'></div>");
+    }
+    
+    var containerWidth = ($("#container").width()) / numSquares;
+    $(".square").css("width", containerWidth);
+    $(".square").css("height", containerWidth);
+
+    //sets the background color of the squares on mouse enter
+    $(".square").mouseenter(function() {
+        $(this).css('background-color', 'blue')
+    })
+
+    //sets the square background to transparent on button click
+    $("#btnclear").click(function() {
+        $(".square").css("background", "transparent")
+    })
+
+
 }
+
 
 //var squareNumber = 1;
-//var rowNumber = 1;
-var sqRoot = Math.sqrt(box);
+//var numSquaresNumber = 1;
+/*
+var sqRoot = Math.sqrt(numSquares);
 var gridHeight = 665;
 var gridWidth = 864;
-var boxHeight = Math.round(gridHeight / sqRoot); 
-var boxWidth =  Math.round(gridWidth / sqRoot);
-
-for(i = 0; i < box; i++) {
-    $("#grid").append("<div class='square'></div>");
-}
-
-$(".square").css("height", boxHeight);
-$(".square").css("width", boxWidth);
+var numSquaresHeight = Math.round(gridHeight / sqRoot); 
+var numSquaresWidth =  Math.round(gridWidth / sqRoot);
+*/
 
 /*
-for(i = 0; i < box; i++) {
+$(".square").css("height", numSquaresHeight);
+$(".square").css("width", numSquaresWidth);
+
+*/
+/*
+for(i = 0; i < numSquares; i++) {
     $("#grid").append("<td class='square'></td>");
 }
 */
 
 //when the mouse enters the div square it will change the background color
-$(".square").mouseenter(function() {
-    $(this).css('background-color', 'blue')
-})
 
-$("#btnclear").click(function() {
-    $(".square").css("background", "transparent")
-})
-
-});
 /*
 var gridSize = $(".square").height();
-$(".square").height(gridSize/box);
+$(".square").height(gridSize/numSquares);
 
 });
 
@@ -52,13 +76,13 @@ $(".square").hover(function() {
 
 
 /*
-for(i = 0; i < box; i++) {
-    $("#grid").append("<div class='row" + rowNumber + "'></div>");
-    for(j = 0; j < box; j++) {
-        $(".row" + rowNumber).append("<div class='square' id='" + squareNumber +"'></div>");
+for(i = 0; i < numSquares; i++) {
+    $("#grid").append("<div class='numSquares" + numSquaresNumber + "'></div>");
+    for(j = 0; j < numSquares; j++) {
+        $(".numSquares" + numSquaresNumber).append("<div class='square' id='" + squareNumber +"'></div>");
         squareNumber ++;
     }
-    rowNumber ++;
+    numSquaresNumber ++;
 }
 
 });
